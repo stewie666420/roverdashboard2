803e944853d3324565557b3d1cfcee9204b62ee6
stewie666420 <officialbenwah@gmail.com>
2025-11-06 13:49:21 -0800

    Update script.js

--- roverdashboard2/script.js
+++ roverdashboard2/script.js
@@ -1,6 +1,6 @@
 const API_KEY = "79f7d0eb3513bf810fb65e4f135014d8";
 const SUBDOMAIN = "roverkennels";
-const LOCATION_ID = "1";
+const LOCATION_ID = "rover-santa-monica";
 
 async function fetchDogs() {
   const url = `https://${SUBDOMAIN}.gingrapp.com/api/v1/back_of_house?key=${API_KEY}&location_id=${LOCATION_ID}&full_day=true`;
@@ -80,3 +80,4 @@ function adjustScrollSpeed(dogCount) {
 // Initial fetch + every minute refresh
 fetchDogs();
 setInterval(fetchDogs, 60000);
+
